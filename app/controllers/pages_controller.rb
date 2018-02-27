88561e5accfa7f920ea52c48c2b3dc102eb8e033
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-02-27 17:02:39 +0100

    adding first/second/third in batches table + batchscore for creators. homepage back for showcased / this week / last week

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,11 @@ class PagesController < ApplicationController
   def home
     @showcased_creator = Creator.where("is_showcased = true")[0]
     current_batch = Batch.where("status = 'active'")[0]
-    last_batch = Batch.where("number = #{current_batch.number - 1}")
+    last_batch = Batch.where("number = #{current_batch.number - 1}")[0]
     # sql active records : @my_bookings = Booking.joins(:performance).where(performances: { user_id: current_user.id }).order(status: :DESC)
     @creators_in_current_batch = Creator.where("batch_id = #{current_batch.id}")
-
+    @creators_in_last_batch = Creator.where("batch_id = #{last_batch.id}")
+    @last_batch_first = Creator.find(last_batch
 
   end
 
