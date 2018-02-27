5c480c3938acd9a1cfd8192c9f8df9fe4f26483a
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-02-27 16:39:16 +0100

    adding number to batches, starting back end for homepage, have a V0 of seed for homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227111859) do
+ActiveRecord::Schema.define(version: 20180227152836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180227111859) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number"
   end
 
   create_table "creators", force: :cascade do |t|
