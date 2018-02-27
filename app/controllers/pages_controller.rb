2bda8fb32b28d0fb11d2c8ce9656ef87e1200a13
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-02-27 17:11:09 +0100

    clean gst before solving conflicts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  CURRENT_BATCH = Batch.where("status = 'active'")[0]
-  LAST_BATCH = Batch.where("number = #{CURRENT_BATCH.number - 1}")
+  # CURRENT_BATCH = Batch.where("status = 'active'")[0]
+  # LAST_BATCH = Batch.where("number = #{CURRENT_BATCH.number - 1}")
 
   def home
     @showcased_creator = Creator.where("is_showcased = true")[0]
@@ -11,7 +11,6 @@ class PagesController < ApplicationController
     # sql active records : @my_bookings = Booking.joins(:performance).where(performances: { user_id: current_user.id }).order(status: :DESC)
     @creators_in_current_batch = Creator.where("batch_id = #{current_batch.id}")
     @creators_in_last_batch = Creator.where("batch_id = #{last_batch.id}")
-    @last_batch_first = Creator.find(last_batch
 
   end
 
