b9b47de5b6107eb591224e7b8deadfb0275d4666
Alexis Pipieri <alexis.pipieri@gmail.com>
2018-02-28 19:30:28 +0100

    end of day

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize @user
   end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :bio, {photos: []})
+  end
 end
