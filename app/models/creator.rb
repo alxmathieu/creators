4eeb5883e43d18b5ef77447805f1fcb06bc64d37
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-28 12:22:02 +0100

    add validation of youtube url

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -2,8 +2,9 @@ class Creator < ApplicationRecord
   belongs_to :user
   belongs_to :batch
   has_many :upvotes
-  validates :channel_url, presence: true, uniqueness: true
-
+  validates :channel_url, presence: true, uniqueness: true, format: { with: /(?:https|http)\:\/\/(?:[\w]+\.)?youtube\.com\/(?:c\/|channel\/|user\/)?([a-zA-Z0-9\-]{1,})/, message: 'Channel url is invalid'}
+  validates :youtube_name, presence: true,
+  validates :description, presence: true,
 
   # Act as Taggable
   acts_as_taggable # Alias for acts_as_taggable_on :tags
@@ -12,3 +13,4 @@ end
 
   # ONLY FOR RUBY 5.2
   # has_many :tag_taggings, class_name: 'ActsAsTaggableOn::Tagging'
+
