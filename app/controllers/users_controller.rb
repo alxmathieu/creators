2ea02091e71ffc90316544a1d672c6b588bf50c7
Alexandre Mathieu <33156152+alxmathieu@users.noreply.github.com>
2018-03-01 10:30:01 +0100

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :bio, avatar:)
+    params.require(:user).permit(:username, :bio, :avatar)
   end
 end
