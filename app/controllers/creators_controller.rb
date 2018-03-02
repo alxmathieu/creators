646ecd3a4f9222c0dd6ee9c32ed2231df9cef8ac
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-03-02 16:11:27 +0100

    fixing bug for adding a new creator

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -16,7 +16,6 @@ class CreatorsController < ApplicationController
     channel_url = params[:creator][:channel_url]
     @youtube_data = ApiScrapper.new(channel_url).scrape
     @creator = Creator.new(@youtube_data.slice(:channel_url, :youtube_name, :avatar_photo, :nb_followers))
-    @creator = Creator.new
     authorize @creator
   end
 
