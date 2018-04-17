70a12f60d63a2a6d0d15f298c0052832706cc954
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-04-17 11:21:20 +0200

    wording on creator modal + comment on confirmable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
+  # used for local sent
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
