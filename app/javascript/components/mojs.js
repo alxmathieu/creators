69ae6d2864897d3947a8fd3b5092e8a7dbcc4e88
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-04-24 16:40:17 +0200

    adding burst effect to the counter

--- app/javascript/components/mojs.js
+++ app/javascript/components/mojs.js
@@ -14,14 +14,37 @@ function getOffset( el ) {
 function showBurst() {
 
   const allUpvoteBtnsInactive = document.querySelectorAll(".icon-not-upvoted");
+
+  // default style
+  // const explo = new mojs.Burst({
+  //   left:0,
+  //   top: 0,
+  //    children: {
+  //      fill:       '#fec624',
+  //    }
+  // });
+
+  //twitter style
   const explo = new mojs.Burst({
-    left:0,
-    top: 0,
+    left: 0, top: 0,
+    radius:   { 4: 32 },
+    angle:    45,
+    count:    14,
+    children: {
+      radius:       2.5,
+      fill:         '#fec624',
+      scale:        { 1: 0, easing: 'quad.in' },
+      pathScale:    [ .8, null ],
+      degreeShift:  [ 13, null ],
+      duration:     [ 500, 700 ],
+      easing:       'quint.out'
+    }
   });
+
   allUpvoteBtnsInactive.forEach((upvoteBtn) => {
     upvoteBtn.addEventListener( 'click', function (e) {
       explo
-        .tune({ x: getOffset(upvoteBtn).left + 0.45 * (upvoteBtn.offsetWidth) , y: getOffset(upvoteBtn).top + + 0.5 * (upvoteBtn.offsetHeight)  })
+        .tune({ x: getOffset(upvoteBtn).left + 0.5 * (upvoteBtn.offsetWidth) , y: getOffset(upvoteBtn).top + + 1.5 * (upvoteBtn.offsetHeight)  }) //0.5 for centering it to the button
         .replay();
       });
     });
