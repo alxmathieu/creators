74c9e0631fb2e48ded8b9af0409cce4f102d5bc7
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-05-16 16:51:59 +0200

    fixing small bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -36,11 +36,9 @@ class PagesController < ApplicationController
       current_user.update_level
     end
 
-    if Batch.next_batch.creators.length < 4
-      @next_creators = ""
-    else
-      @next_creators = Batch.next_batch.creators.length
-    end
+
+      @next_creators = Batch.next_batch.creators
+
 
   end
 
